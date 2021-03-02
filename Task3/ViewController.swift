67b2ba568013fc5b60591a13d69c6d0f3d8b068a
daigo <daigon.0114@gmail.com>
2021-03-02 14:56:22 +0900

    フィードバック後、修正

--- Task3/ViewController.swift
+++ Task3/ViewController.swift
@@ -16,20 +16,33 @@ class ViewController: UIViewController {
     @IBOutlet private var textLabel1: UILabel!
     @IBOutlet private var textLabel2: UILabel!
     @IBOutlet private var resultLabel: UILabel!
+
     @IBAction private func caluclationButton(_ sender: Any) {
         guard let text1 = textField1.text else { return }
         guard let text2 = textField2.text else { return }
-        var textNum1 = Int(text1) ?? 0
-        var textNum2 = Int(text2) ?? 0
+
+        let textNum1 = Int(text1) ?? 0
+        let textNum2 = Int(text2) ?? 0
+
+        let signedNum1: Int
         if switchButton.isOn {
-            textNum1 *= -1
+            signedNum1 = -textNum1
+        } else {
+            signedNum1 = textNum1
         }
+
+        let signedNum2: Int
         if switchButton2.isOn {
-            textNum2 *= -1
+            signedNum2 = -textNum2
+        } else {
+            signedNum2 = textNum2
         }
-        textLabel1.text = String(textNum1)
-        textLabel2.text = String(textNum2)
-        let resultNum = textNum1 + textNum2
+
+        textLabel1.text = String(signedNum1)
+        textLabel2.text = String(signedNum2)
+
+        let resultNum = signedNum1 + signedNum2
+
         resultLabel.text = String(resultNum)
     }
 }
